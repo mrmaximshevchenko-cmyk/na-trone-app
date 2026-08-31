46772b41ec5015d9bc02c42646c28206066a7701
AimBet <mr.maximshevchenko@gmail.com>
2026-08-31 23:36:05 +0300

    Ждём загрузку Telegram SDK перед стартом

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-const tg = (window as any).Telegram?.WebApp
-if (tg) {
-  tg.ready()
-  tg.expand()
+function startApp() {
+  const tg = (window as any).Telegram?.WebApp
+  if (tg) {
+    tg.ready()
+    tg.expand()
+  }
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
 }
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Ждём, пока загрузится Telegram SDK, потом стартуем
+if ((window as any).Telegram?.WebApp) {
+  startApp()
+} else {
+  const script = document.createElement('script')
+  script.src = 'https://telegram.org/js/telegram-web-app.js'
+  script.onload = startApp
+  script.onerror = startApp
+  document.head.appendChild(script)
+}
